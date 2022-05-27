d440dc6da159cbea29f5d02e87cbdcbf71218144
Jon2G <jonyjouna@gmail.com>
2022-05-26 19:43:37 -0500

    Add kit dependency

---
+++ EuclidsPolynomialsEqSolver/DiscreteMaths/PolynomialEq.cs
@@ -0,0 +1,23 @@
+﻿using Kit;
+using System.Text;
+namespace DiscreteMaths
+{
+    public class PolynomialEq
+    {
+        public XTerm[] Terms { get; set; }
+        public PolynomialEq()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (XTerm term in Terms)
+            {
+                sb.Append($" {term} +");
+            }
+            return sb.RemoveLast().ToString();
+        }
+    }
+}
---
+++ EuclidsPolynomialsEqSolver/DiscreteMaths/XTerm.cs
@@ -0,0 +1,18 @@
+﻿namespace DiscreteMaths
+{
+    public class XTerm
+    {
+        public int Exponent { get; set; }
+        public int Value { get; set; }
+
+        public XTerm()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Value}x^{Exponent}";
+        }
+    }
+}
